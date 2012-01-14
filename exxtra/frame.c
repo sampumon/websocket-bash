f737494c4ed885e6e2b0611f2d9b4c714c0f1795
sampula <samuli.kaipiainen@helsinki.fi>
2012-01-14 14:24:56 +0200

    frame unmasking prototype

--- exxtra/frame.c
+++ exxtra/frame.c
@@ -3,19 +3,20 @@
 int main() {
 	char s[1024], o[1024];
 	int i; 
-	unsigned int maskint;
-	char *mask = &maskint;
+	// unsigned int maskint;
+	char *mask = s+2;
+	char *payload = s+6;
 
 	while (fgets(s, 1024, stdin)) {
 
-		maskint = ((unsigned int *) s)[5];
-		printf("mask: %u\n", maskint);
+		// maskint = ((unsigned int *) s)[5];
+		// printf("mask: %u\n", maskint);
 		printf("mask: %4c\n", mask);
 
-		for (i=0; s[i]; i++) {
-			o[i] = s[i] ^ mask[i % 4];
+		for (i=0; payload[i]; i++) {
+			o[i] = payload[i] ^ mask[i % 4];
 		}
-		o[i] = s[i];
+		o[i] = payload[i];
 
 		fputs(o, stdout);
 	}
